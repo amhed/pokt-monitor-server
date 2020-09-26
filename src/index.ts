46ad2e27d75a709783216f41befcd0919bcb369e
amhed <aherrera@gmail.com>
2020-09-26 12:17:55 -0500

    blip

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-const https = require('https')
-const fs = require('fs')
-
 import app from './server'
 import { Logger } from './helpers/Logger'
 
-var options = {
-  key: fs.readFileSync( './server.key' ),
-  cert: fs.readFileSync( './server.cert' ),
-  requestCert: false,
-  rejectUnauthorized: false
-};
-
-const port = process.env.PORT || 3000;
-const server = https.createServer( options, app );
-
-server.listen(port, () => {
-  console.log( 'Express server listening on port ' + server.address().port );
-} );
+const port = Number(process.env.PORT || 3000)
+app.listen(port, () => {
+  Logger.info('Express server started on port: ' + port)
+})
