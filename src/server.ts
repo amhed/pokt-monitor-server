a432fe56d367f6053c98de6a45dabec861b9838b
amhed <aherrera@gmail.com>
2020-09-26 11:14:56 -0500

    ffff

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ configureViews(app)
 // Routes
 app.use(cors())
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
