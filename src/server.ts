56e8360032de2b13846be9d34eea6bb549a88322
amhed <aherrera@gmail.com>
2020-09-26 11:13:14 -0500

    ff

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,12 @@ configureViews(app)
 
 // Routes
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/stats', statsController);
 
 // The Sentry error handler must be before any other error 
