27a8827e40b424e0a658fd4e99ef0ba35e488f6b
amhed <aherrera@gmail.com>
2020-09-26 11:10:12 -0500

    Access-Control-Allow-Origin

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Views
 configureViews(app)
 
