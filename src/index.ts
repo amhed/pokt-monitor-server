7a160c18e97a11f80c56de16ced03fe07a243bc6
amhed <aherrera@gmail.com>
2020-09-26 12:02:25 -0500

    Enable https

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
+const https = require('https')
+const fs = require('fs')
+
 import app from './server'
 import { Logger } from './helpers/Logger'
 
-const port = Number(process.env.PORT || 3000)
-app.listen(port, () => {
-  Logger.info('Express server started on port: ' + port)
-})
+var options = {
+  key: fs.readFileSync( './server.key' ),
+  cert: fs.readFileSync( './server.cert' ),
+  requestCert: false,
+  rejectUnauthorized: false
+};
+
+const port = process.env.PORT || 3000;
+const server = https.createServer( options, app );
+
+server.listen(port, () => {
+  console.log( 'Express server listening on port ' + server.address().port );
+} );
